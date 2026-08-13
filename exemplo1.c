b575d702d3ec1a19c84e2863cbc05133111457a5
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2026-08-13 23:04:52 +0000

    ex1

---
+++ exemplo1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+    printf("Mackenzie - FCI\n");
+    printf("Algoritimos  e ");
+    printf("Prog. II\n");
+    printf("Turma 02P");
+    
+    system("pause");
+    return 0;
+}
---
+++ exemplo2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+    printf("Mackenzie - FCI\n");
+    printf("Algoritimos  e ");
+    printf("Prog. II\n");
+    printf("Turma 02P\n");
+    printf("FCI fundada em 2018");
+
+    system("pause");
+    return 0;
+}
